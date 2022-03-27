f5ad5c35c4bb6378ad920b28edbeef13226de4f4
FriendlyFire <jpguertin.cg@gmail.com>
2022-03-27 16:22:25 -0400

    Migrating common code.

---
+++ include/EnumUtils.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <type_traits>
+
+template<typename T>
+concept enum_with_none = requires(T && t) {
+	requires std::is_enum_v<T>;
+	T::NONE;
+};
+
+template<enum_with_none Enum>
+constexpr bool notNone(Enum e) {
+	return e != Enum::NONE;
+}
+
+template<enum_with_none Enum>
+constexpr bool isNone(Enum e) {
+	return e == Enum::NONE;
+}
