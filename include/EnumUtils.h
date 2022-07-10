2822602d3b198270ee61d0aee1f93437ad92c099
FriendlyFire <jpguertin.cg@gmail.com>
2022-07-10 16:42:54 -0400

    Added enum iteration.

--- include/EnumUtils.h
+++ include/EnumUtils.h
@@ -7,6 +7,12 @@ concept enum_with_none = requires(T && t) {
 	T::NONE;
 };
 
+template<typename T>
+concept iteratable_enum = requires(T && t) {
+	requires std::is_enum_v<T>;
+	T::FIRST <= T::LAST;
+};
+
 template<enum_with_none Enum>
 constexpr bool notNone(Enum e) {
 	return e != Enum::NONE;
@@ -15,4 +21,10 @@ constexpr bool notNone(Enum e) {
 template<enum_with_none Enum>
 constexpr bool isNone(Enum e) {
 	return e == Enum::NONE;
+}
+
+template<iteratable_enum E>
+void iterateEnum(std::function<void(E)> cb) {
+	for (auto i = E::FIRST; i <= E::LAST; i = static_cast<E>(std::underlying_type_t<E>(i) + 1))
+		cb(i);
 }
