74be39c20df1c73a890c95143b71a6f77b5f95ad
FriendlyFire <jpguertin.cg@gmail.com>
2022-08-02 23:36:32 -0400

    Adding differentiation between underwater and on water to MumbleLink ConditionalState.

--- include/MumbleLink.h
+++ include/MumbleLink.h
@@ -9,160 +9,188 @@ struct MumbleContext;
 
 enum class ConditionalState : uint
 {
-	NONE = 0,
-	UNDERWATER = 1,
-	IN_COMBAT = 2,
-	IN_WVW = 4,
+    NONE = 0,
+    UNDERWATER = 1,
+    ON_WATER = 2,
+    IN_COMBAT = 4,
+    IN_WVW = 8,
 
-	ALL = UNDERWATER | IN_COMBAT | IN_WVW
+    ALL = UNDERWATER | ON_WATER | IN_COMBAT | IN_WVW
 };
 
-inline ConditionalState operator|(ConditionalState a, ConditionalState b) { return ConditionalState(uint(a) | uint(b)); }
-inline ConditionalState operator&(ConditionalState a, ConditionalState b) { return ConditionalState(uint(a) & uint(b)); }
-inline ConditionalState operator~(ConditionalState a) { return ConditionalState(~uint(a)); }
+inline ConditionalState operator|(ConditionalState a, ConditionalState b)
+{
+    return static_cast<ConditionalState>(uint(a) | uint(b));
+}
 
-class MumbleLink : public Singleton<MumbleLink> {
+inline ConditionalState operator&(ConditionalState a, ConditionalState b)
+{
+    return static_cast<ConditionalState>(uint(a) & uint(b));
+}
+
+inline ConditionalState operator~(ConditionalState a)
+{
+    return static_cast<ConditionalState>(~uint(a));
+}
+
+class MumbleLink : public Singleton<MumbleLink>
+{
 public:
-	enum class Profession : uint8_t {
-		NONE = 0,
-	    GUARDIAN = 1,
-		WARRIOR = 2,
-		ENGINEER = 3,
-		RANGER = 4,
-		THIEF = 5,
-		ELEMENTALIST = 6,
-		MESMER = 7,
-		NECROMANCER = 8,
-		REVENANT = 9
-	};
+    enum class Profession : uint8_t
+    {
+        NONE = 0,
+        GUARDIAN = 1,
+        WARRIOR = 2,
+        ENGINEER = 3,
+        RANGER = 4,
+        THIEF = 5,
+        ELEMENTALIST = 6,
+        MESMER = 7,
+        NECROMANCER = 8,
+        REVENANT = 9
+    };
 
-	enum class EliteSpec : uint8_t {
-		NONE = 0,
-		BERSERKER = 1,
-		BLADESWORN = 2,
-		CATALYST = 3,
-		CHRONOMANCER = 4,
-		DAREDEVIL = 5,
-		DEADEYE = 6,
-		DRAGONHUNTER = 7,
-		DRUID = 8,
-		FIREBRAND = 9,
-		HARBINGER = 10,
-		HERALD = 11,
-		HOLOSMITH = 12,
-		MECHANIST = 13,
-		MIRAGE = 14,
-		REAPER = 15,
-		RENEGADE = 16,
-		SCOURGE = 17,
-		SCRAPPER = 18,
-		SOULBEAST = 19,
-		SPECTER = 20,
-		SPELLBREAKER = 21,
-		TEMPEST = 22,
-		UNTAMED = 23,
-		VINDICATOR = 24,
-		VIRTUOSO = 25,
-		WEAVER = 26,
-		WILLBENDER = 27
-	};
+    enum class EliteSpec : uint8_t
+    {
+        NONE = 0,
+        BERSERKER = 1,
+        BLADESWORN = 2,
+        CATALYST = 3,
+        CHRONOMANCER = 4,
+        DAREDEVIL = 5,
+        DEADEYE = 6,
+        DRAGONHUNTER = 7,
+        DRUID = 8,
+        FIREBRAND = 9,
+        HARBINGER = 10,
+        HERALD = 11,
+        HOLOSMITH = 12,
+        MECHANIST = 13,
+        MIRAGE = 14,
+        REAPER = 15,
+        RENEGADE = 16,
+        SCOURGE = 17,
+        SCRAPPER = 18,
+        SOULBEAST = 19,
+        SPECTER = 20,
+        SPELLBREAKER = 21,
+        TEMPEST = 22,
+        UNTAMED = 23,
+        VINDICATOR = 24,
+        VIRTUOSO = 25,
+        WEAVER = 26,
+        WILLBENDER = 27
+    };
 
-	enum class Race : uint8_t {
-	    ASURA = 0,
-		CHARR = 1,
-		HUMAN = 2,
-		NORN = 3,
-		SYLVARI = 4
-	};
+    enum class Race : uint8_t
+    {
+        ASURA = 0,
+        CHARR = 1,
+        HUMAN = 2,
+        NORN = 3,
+        SYLVARI = 4
+    };
 
-	enum class MountType : uint32_t {
-		NONE,
-		JACKAL,
-		GRIFFON,
-		SPRINGER,
-		SKIMMER,
-		RAPTOR,
-		ROLLER_BEETLE,
-		WARCLAW,
-		SKYSCALE,
-		SKIFF,
-		SIEGE_TURTLE
-	};
+    enum class MountType : uint32_t
+    {
+        NONE,
+        JACKAL,
+        GRIFFON,
+        SPRINGER,
+        SKIMMER,
+        RAPTOR,
+        ROLLER_BEETLE,
+        WARCLAW,
+        SKYSCALE,
+        SKIFF,
+        SIEGE_TURTLE
+    };
 
-	MumbleLink();
-	~MumbleLink();
+    MumbleLink();
+    ~MumbleLink() override;
 
-	void OnUpdate();
+    void OnUpdate();
 
-	[[nodiscard]] bool isInWvW() const;
+    [[nodiscard]] bool isInWvW() const;
 
-	// uiState flags
-	PARSE_FLAG_BOOL(isMapOpen, 0);
-	PARSE_FLAG_BOOL(isCompassTopRight, 1);
-	PARSE_FLAG_BOOL(doesCompassHaveRotationEnabled, 2);
-	PARSE_FLAG_BOOL(gameHasFocus, 3);
-	PARSE_FLAG_BOOL(isInCompetitiveMode, 4);
-	PARSE_FLAG_BOOL(textboxHasFocus, 5);
-	PARSE_FLAG_BOOL(isInCombat, 6);
+    // uiState flags
+    PARSE_FLAG_BOOL(isMapOpen, 0);
+    PARSE_FLAG_BOOL(isCompassTopRight, 1);
+    PARSE_FLAG_BOOL(doesCompassHaveRotationEnabled, 2);
+    PARSE_FLAG_BOOL(gameHasFocus, 3);
+    PARSE_FLAG_BOOL(isInCompetitiveMode, 4);
+    PARSE_FLAG_BOOL(textboxHasFocus, 5);
+    PARSE_FLAG_BOOL(isInCombat, 6);
 
-	[[nodiscard]] fVector3 position() const;
+    [[nodiscard]] fVector3 position() const;
 
-	[[nodiscard]] MountType currentMount() const;
-	[[nodiscard]] bool isMounted() const;
-	[[nodiscard]] bool isInMap() const;
-	[[nodiscard]] uint32_t mapId() const;
-	[[nodiscard]] std::wstring characterName() const;
-	[[nodiscard]] bool isSwimmingOnSurface() const;
-	[[nodiscard]] bool isUnderwater() const;
-	[[nodiscard]] bool isOnOrUnderwater() const { return isSwimmingOnSurface() || isUnderwater(); }
+    [[nodiscard]] MountType    currentMount() const;
+    [[nodiscard]] bool         isMounted() const;
+    [[nodiscard]] bool         isInMap() const;
+    [[nodiscard]] uint32_t     mapId() const;
+    [[nodiscard]] std::wstring characterName() const;
+    [[nodiscard]] bool         isSwimmingOnSurface() const;
+    [[nodiscard]] bool         isUnderwater() const;
 
-	[[nodiscard]] ConditionalState currentState() const {
-		return (isOnOrUnderwater() ? ConditionalState::UNDERWATER : ConditionalState::NONE) |
-			(isInCombat() ? ConditionalState::IN_COMBAT : ConditionalState::NONE) |
-			(isInWvW() ? ConditionalState::IN_WVW : ConditionalState::NONE);
-	}
-
-	[[nodiscard]] Profession characterProfession() const {
-	    return identity_.profession;
+    [[nodiscard]] bool isOnOrUnderwater() const
+    {
+        return isSwimmingOnSurface() || isUnderwater();
     }
 
-	[[nodiscard]] EliteSpec characterSpecialization() const;
-
-    [[nodiscard]] Race characterRace() const {
-	    return identity_.race;
+    [[nodiscard]] ConditionalState currentState() const
+    {
+        return (isSwimmingOnSurface() ? ConditionalState::ON_WATER : ConditionalState::NONE) |
+               (isUnderwater() ? ConditionalState::UNDERWATER : ConditionalState::NONE) |
+               (isInCombat() ? ConditionalState::IN_COMBAT : ConditionalState::NONE) |
+               (isInWvW() ? ConditionalState::IN_WVW : ConditionalState::NONE);
     }
 
-	[[nodiscard]] bool isCommander() const {
-		return identity_.commander;
-	}
+    [[nodiscard]] Profession characterProfession() const
+    {
+        return identity_.profession;
+    }
 
-	[[nodiscard]] float fov() const {
-		return identity_.fov;
-	}
+    [[nodiscard]] EliteSpec characterSpecialization() const;
 
-	[[nodiscard]] uint8_t uiScale() const {
-		return identity_.uiScale;
-	}
+    [[nodiscard]] Race characterRace() const
+    {
+        return identity_.race;
+    }
+
+    [[nodiscard]] bool isCommander() const
+    {
+        return identity_.commander;
+    }
+
+    [[nodiscard]] float fov() const
+    {
+        return identity_.fov;
+    }
+
+    [[nodiscard]] uint8_t uiScale() const
+    {
+        return identity_.uiScale;
+    }
 
 protected:
-	std::wstring fileMappingName_ = L"MumbleLink";
+    std::wstring fileMappingName_ = L"MumbleLink";
 
-	[[nodiscard]] uint32_t uiState() const;
+    [[nodiscard]] uint32_t uiState() const;
 
-	struct Identity
-	{
-	    Profession profession = Profession::NONE;
-		EliteSpec specialization = EliteSpec::NONE;
-		Race race = Race::ASURA;
-		bool commander = false;
-		float fov = 0.f;
-		uint8_t uiScale = 0;
-		std::string name;
-	};
+    struct Identity
+    {
+        Profession  profession     = Profession::NONE;
+        EliteSpec   specialization = EliteSpec::NONE;
+        Race        race           = Race::ASURA;
+        bool        commander      = false;
+        float       fov            = 0.f;
+        uint8_t     uiScale        = 0;
+        std::string name;
+    };
 
-	[[nodiscard]] const MumbleContext* context() const;
-	HANDLE fileMapping_ = nullptr;
-	LinkedMem* linkedMemory_ = nullptr;
+    [[nodiscard]] const MumbleContext* context() const;
+    HANDLE                             fileMapping_  = nullptr;
+    LinkedMem*                         linkedMemory_ = nullptr;
 
-	Identity identity_;
+    Identity identity_;
 };
