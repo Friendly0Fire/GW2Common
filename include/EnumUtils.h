b9eae90dd19be899d0959bddbaa5a61d556ac69e
FriendlyFire <jpguertin.cg@gmail.com>
2023-08-09 00:38:40 -0400

    Added EnumSize.

--- include/EnumUtils.h
+++ include/EnumUtils.h
@@ -5,10 +5,23 @@ template<typename T>
 concept Enum = std::is_enum_v<T>;
 
 template<typename T>
-concept EnumWithNone = Enum<T> && requires(T e) { static_cast<size_t>(T::None) == 0; };
+concept EnumWithNone = Enum<T> && requires() { static_cast<size_t>(T::None) == 0; };
 
 template<typename T>
-concept IteratableEnum = Enum<T> && requires(T e) { T::First <= T::Last; };
+concept IteratableEnum = Enum<T> && requires() { T::First <= T::Last; };
+
+template<typename T>
+concept SizedEnum = Enum<T> && requires() { T::Count; };
+
+template<SizedEnum T>
+constexpr auto EnumSize() {
+    return std::to_underlying(T::Count);
+}
+
+template<IteratableEnum T>
+constexpr auto EnumSize() {
+    return std::to_underlying(T::Last) - std::to_underlying(T::First) + 1;
+}
 
 template<EnumWithNone T>
 constexpr bool NotNone(T e) {
